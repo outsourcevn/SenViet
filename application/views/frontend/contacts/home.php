d5197b9cc74a9872a32bc8719860c6bc7af023dd
Duc Ta Minh <ductm1@smartosc.com>
2015-11-11 16:42:46 +0700

    Change location Gmaps

--- application/views/frontend/contacts/home.php
+++ application/views/frontend/contacts/home.php
@@ -1,4 +1,4 @@
-<div class="content">
+﻿<div class="content">
     <div class="container">
         <ol class="breadcrumb">
             <li><a href="<?php echo base_url();?>"><i>Trang chủ</i></a></li>
@@ -110,7 +110,7 @@
                     </div>
                 </form>
             </div>
-            <div class="col-lg-6"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d931.1782851427769!2d105.8390258270572!3d21.004132975263936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac7995e278d3%3A0x96d1ecb492501ce1!2zMTUgUGjGsMahbmcgTWFpLCDEkOG7kW5nIMSQYSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1441908015653" width="100%" height="260" frameborder="0" style="border:0" allowfullscreen></iframe></div>
+            <div class="col-lg-6"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.0872662595825!2d105.76678931423663!3d21.0291939859981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjHCsDAxJzQ1LjEiTiAxMDXCsDQ2JzA4LjMiRQ!5e0!3m2!1svi!2ssg!4v1447234785063" width="100%" height="260" frameborder="0" style="border:0" allowfullscreen></iframe></div>
         </div>
     </div>
 </div>
